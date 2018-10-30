1711a191a5ac80613b535da2c0d9c28eb8874b47
Masato Ikeda <masato.ikeda@gmail.com>
2018-10-31 02:01:42 +0900

    Refactor test code with macro

--- test/run_loop_test.c
+++ test/run_loop_test.c
@@ -2,22 +2,27 @@
 
 char *read_line(FILE *in);
 
+#define EXPECT(expected, actual)                            \
+  do {                                                      \
+    char *e = (expected);                                   \
+    char *a = (actual);                                     \
+    if (strcmp(e, a) != 0) {                                \
+      fprintf(stderr, "line %d: expected %s, but got %s\n", \
+          __LINE__, e, a);                                  \
+      exit(1);                                              \
+    }                                                       \
+  } while(0)
+
 int main(int argc, char **argv) {
   char *line;
   FILE *f = fopen("test/lines.txt", "r");
 
   line = read_line(f);
-  if (strcmp("1111", line) != 0) {
-    fprintf(stderr, "expected \"1111\" but got \"%s\"\n", line);
-    exit(1);
-  }
+  EXPECT("1111", line);
   free(line);
 
   line = read_line(f);
-  if (strcmp("2222 3333", line) != 0) {
-    fprintf(stderr, "expected \"2222 3333\" but got \"%s\"\n", line);
-    exit(1);
-  }
+  EXPECT("2222 3333", line);
   free(line);
 
   fclose(f);
