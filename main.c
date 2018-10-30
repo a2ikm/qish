a401f727eec9824a6d803780f430c73bd43f1bf6
Masato Ikeda <masato.ikeda@gmail.com>
2018-10-30 21:27:41 +0900

    Print each input line

--- main.c
+++ main.c
@@ -1,5 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define QISH_LINE_BUFSIZE 256
+
+char *read_line(void) {
+  int ch;
+  int pos = 0;
+  int bufsize = QISH_LINE_BUFSIZE;
+  char *buffer = malloc(sizeof(char) * bufsize);
+
+  if (buffer == NULL) {
+    fprintf(stderr, "qish: memory allocation error\n");
+    exit(1);
+  }
+
+  while (1) {
+    ch = getchar();
+    if (ch == EOF || ch == '\n') {
+      buffer[pos] = '\0';
+      return buffer;
+    }
+
+    buffer[pos] = ch;
+    pos++;
+
+    if (pos >= bufsize) {
+      bufsize *= 2;
+      buffer = realloc(buffer, bufsize);
+      if (buffer == NULL) {
+        fprintf(stderr, "qish: memory allocation error\n");
+        exit(1);
+      }
+    }
+  }
+}
 
 int main(int argc, const char **args) {
+  char *line;
+
+  while(1) {
+    printf("> ");
+    line = read_line();
+    printf("%s\n", line);
+    free(line);
+  }
   return 0;
 }
